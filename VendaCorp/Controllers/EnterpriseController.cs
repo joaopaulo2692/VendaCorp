c46f1e2043884d695a6998df7326d861cd8e26f7
joaop <joaopaulo2692@gmail.com>
2024-08-11 17:03:13 -0300

    feat - implementado controller de Enterprise

---
+++ VendaCorp/Controllers/EnterpriseController.cs
@@ -0,0 +1,89 @@
+﻿using FluentResults;
+using Microsoft.AspNetCore.Mvc;
+using System.Security.Claims;
+using VendaCorp.Application.DTO;
+using VendaCorp.Application.DTO.Enterprise;
+using VendaCorp.Core.Interfaces.Repositories;
+using VendaCorp.Core.Interfaces.Services;
+
+namespace VendaCorp.API.Controllers
+{
+    public class EnterpriseController : ControllerBase
+    {
+        private readonly IEnterpriseService _enterpriseService;
+
+        public EnterpriseController(IEnterpriseService enterpriseService)
+        {
+            _enterpriseService = enterpriseService;
+        }
+
+        [HttpPost]
+        [Route("Create")]
+        public async Task<IActionResult> CreateAsync([FromBody] EnterpriseVO model)
+        {
+            try
+            {
+                Claim idUser = User.FindFirst(ClaimTypes.NameIdentifier);
+                if (idUser == null)
+                {
+                    return StatusCode(StatusCodes.Status401Unauthorized);
+                }
+
+                Result response = await _enterpriseService.CreateAsync(model);
+                if (response.IsFailed) return StatusCode(StatusCodes.Status400BadRequest);
+
+                return StatusCode(StatusCodes.Status201Created);
+            }
+            catch (Exception ex)
+            {
+                return StatusCode(StatusCodes.Status500InternalServerError);
+            }
+        }
+
+        [HttpPost]
+        [Route("Activate")]
+        public async Task<IActionResult> ActivateAsync(int id)
+        {
+            try
+            {
+                Claim idUser = User.FindFirst(ClaimTypes.NameIdentifier);
+                if (idUser == null)
+                {
+                    return StatusCode(StatusCodes.Status401Unauthorized);
+                }
+
+                Result response = await _enterpriseService.ActivateAsync(id);
+                if (response.IsFailed) return StatusCode(StatusCodes.Status400BadRequest);
+
+                return StatusCode(StatusCodes.Status201Created);
+            }
+            catch (Exception ex)
+            {
+                return StatusCode(StatusCodes.Status500InternalServerError);
+            }
+        }
+
+        [HttpPost]
+        [Route("Disable")]
+        public async Task<IActionResult> DisableAsync(int id)
+        {
+            try
+            {
+                Claim idUser = User.FindFirst(ClaimTypes.NameIdentifier);
+                if (idUser == null)
+                {
+                    return StatusCode(StatusCodes.Status401Unauthorized);
+                }
+
+                Result response = await _enterpriseService.DisableAsync(id);
+                if (response.IsFailed) return StatusCode(StatusCodes.Status400BadRequest);
+
+                return StatusCode(StatusCodes.Status201Created);
+            }
+            catch (Exception ex)
+            {
+                return StatusCode(StatusCodes.Status500InternalServerError);
+            }
+        }
+    }
+}
